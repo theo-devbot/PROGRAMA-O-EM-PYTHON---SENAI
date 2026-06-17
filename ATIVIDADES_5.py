1fd1f318ed7f12afd16c62418a3df99e5cb7a17f
theo-dev <61585071+theo-devbot@users.noreply.github.com>
2026-06-17 14:32:05 -0300

    Add files via upload

---
+++ ATIVIDADES_5.py
@@ -0,0 +1,36 @@
+#ATIVIDADES
+#1 Imprima uma mensagem de boas-vindas na tela.
+print('Bem vindo!')
+
+#2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+Verdade = True
+print(Verdade)
+
+#3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+print(5*5)
+
+#4 Imprima o resultado da divisão (/)de dois números inteiros de sua escolha.
+print(5/5)
+
+#5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+print(20-10)
+
+#6 Imprima o resultado da divisão (//)inteira de dois números inteiros de sua escolha.
+print(1000//10)
+
+#7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha
+print(10*2, 10*3, 10*4, 10*5)
+
+#8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número
+numero = 1000
+print(numero*2)
+
+#9 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+nome = input('Qual é o seu nome? ')
+email = input('Qual é o seu email? ')
+idade = input('Qual é a sua idade? ')
+cidade = input('Qual é a sua cidade? ')
+graduacao = input('Qual é a sua graduação? ')
+estado_civil = input('Qual é o seu estado civil? ')
+
+print(nome, '---', email, '---', idade, '---', cidade, '---', graduacao, '---', estado_civil, '---')
